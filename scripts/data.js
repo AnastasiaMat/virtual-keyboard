491ce9e58fcbedbf91a7baa24750d15bde82392c
AnastasiaMat <anvictma@gmail.com>
2023-04-30 23:02:58 +0300

    feat: add data - russian symbols

--- scripts/data.js
+++ scripts/data.js
@@ -17,3 +17,23 @@ const SIMBOLS_EN_4 = [
 const SIMBOLS_EN_5 = [
   'ctrl l', 'win', 'alt l', 'space', 'alt r', 'ctrl r', 'left', 'down', 'right'
 ];
+
+const SIMBOLS_RU_1 = [
+  'ё', ['1', '!'], ['2', '"'], ['3', '№'], ['4', ';'], ['5', '%'], ['6', ':'], ['7', '?'], ['8', '*'], ['9', '('], ['0', ')'], ['-', '_'], ['=', '+'], ['backspace']
+];
+
+const SIMBOLS_RU_2 = [
+  'tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', [' \ ', '|']
+];
+
+const SIMBOLS_RU_3 = [
+  'caps lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', [`'`, '"'], 'enter'
+];
+
+const SIMBOLS_RU_4 = [
+  'shift l', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', ['.', ','], 'up', 'sifth r'
+];
+
+const SIMBOLS_RU_5 = [
+  'ctrl l', 'win', 'alt l', 'space', 'alt r', 'ctrl r', 'left', 'down', 'right'
+];
