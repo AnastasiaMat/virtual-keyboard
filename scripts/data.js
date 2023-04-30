301c3ab18f1b06b39736a36c0a018f0e31c517b8
AnastasiaMat <anvictma@gmail.com>
2023-04-30 22:57:15 +0300

    feat: add data - english symbols

---
+++ scripts/data.js
@@ -0,0 +1,19 @@
+const SIMBOLS_EN_1 = [
+  ['`', '`'], ['1', '!'], ['2', '@'], ['3', '#'], ['4', '$'], ['5', '%'], ['6', '^'], ['7', '&'], ['8', '*'], ['9', '('], ['0', ')'], ['-', '_'], ['=', '+'], ['backspace']
+];
+
+const SIMBOLS_EN_2 = [
+  'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', ['[', '{'], [']', '}'], [' \ ', '|']
+];
+
+const SIMBOLS_EN_3 = [
+  'caps lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', [';', ':'], [`'`, '"'], 'enter'
+];
+
+const SIMBOLS_EN_4 = [
+  'shift l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', [',', '<'], ['.', '>'], ['/', '?'], 'up', 'sifth r'
+];
+
+const SIMBOLS_EN_5 = [
+  'ctrl l', 'win', 'alt l', 'space', 'alt r', 'ctrl r', 'left', 'down', 'right'
+];
